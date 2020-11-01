c4b00c62a88e5faab9932bb0770cf96a8746fa72
Danik <daniknewgarden@gmail.com>
2020-11-01 19:18:48 +0200

    Aded styles for Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
+
 //Redux
 import { useSelector, useDispatch } from "react-redux";
 import { applyFullscreen } from "../../redux/fullscreen/fullscreenActions";
+import { applyTheme } from "../../redux/theme/themeActions";
 //Components
 import { ControlBtn } from "../ControlBtn/ControlBtn";
 //Styles
@@ -9,31 +11,50 @@ import "./Header.scss";
 //Icons
 import maximize from "../../icons/maximize.svg";
 import minimize from "../../icons/minimize.svg";
+import down from "../../icons/down.svg";
+import up from "../../icons/up.svg";
 
-export const Header = ({}) => {
+export const Header = () => {
   const dispatch = useDispatch();
 
-  //Fullscreen button
   //Screen mode
   const fullscreen = useSelector((state) => state.fullscreen.fullscreen);
   const changeMode = (fullscreen) => {
     dispatch(applyFullscreen(fullscreen));
   };
 
-  const btnLabel = fullscreen ? "Minimize" : "Fullscreen";
+  //Dark theme
+  const darkTheme = useSelector((state) => state.theme.darkTheme);
+  const changeTheme = (theme) => {
+    dispatch(applyTheme(theme));
+  };
+
+  //Control buttons labels
+  const modeLabel = fullscreen ? "Minimize" : "Fullscreen";
+  const themeLabel = darkTheme ? "Dark Theme" : "Light Theme";
 
   return (
-    <header className={`header`}>
-      <a className="header__link" href="#">
+    <header className={`header ${fullscreen ? "fullscreen" : ""}`}>
+      <p className="header__link" href="">
         cr.fetbiko.ru
-      </a>
-      {fullscreen && <ControlBtn />}
-      <ControlBtn
-        label={btnLabel}
-        icon={maximize}
-        iconEnabled={minimize}
-        onClick={() => changeMode(!fullscreen)}
-      />
+      </p>
+      <div className="header__controls">
+        {fullscreen && (
+          <ControlBtn
+            label={themeLabel}
+            icon={down}
+            iconEnabled={up}
+            reversed={true}
+            onClick={() => changeTheme(!darkTheme)}
+          />
+        )}
+        <ControlBtn
+          label={modeLabel}
+          icon={maximize}
+          iconEnabled={minimize}
+          onClick={() => changeMode(!fullscreen)}
+        />
+      </div>
     </header>
   );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,15 @@ import thunk from "redux-thunk";
 //Reducer
 import combineReducers from "./rootReducer";
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV === `development`) {
+  const { logger } = require(`redux-logger`);
+
+  middlewares.push(logger);
+}
+
 //Store
-const store = createStore(combineReducers, applyMiddleware(thunk));
+const store = createStore(combineReducers, applyMiddleware(...middlewares));
 
 export default store;
